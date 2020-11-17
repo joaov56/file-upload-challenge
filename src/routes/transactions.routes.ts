c61e752ca70372a00a32279c72a40a5fd05e97fc
João Vitor <joao@MacBook-Pro-de-Mac-2.local>
2020-11-16 23:48:23 -0300

    adiciona cateogiras e adiciona delete de transacoes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,52 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const balance = await transactionsRepository.getBalance();
+
+  const transactions = await transactionsRepository.find();
+
+  const object = {
+    transactions,
+    balance,
+  };
+  return response.json(object);
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, type, value, category } = request.body;
+  const transaction = new CreateTransactionService();
+
+  const create = await transaction.execute({
+    title,
+    type,
+    value,
+    category,
+  });
+
+  return response.json(create);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const transaction = new DeleteTransactionService();
+
+  const deleted = await transaction.execute(id);
+
+  return response.json(deleted);
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.post('/import', async (request, response) => {
+//   // TODO
+// });
 
 export default transactionsRouter;
